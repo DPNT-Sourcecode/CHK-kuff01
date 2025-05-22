2167609a2734dafb5281c7105983f97a9ea8a469
sbx_user1051 <sbx_user1051@169.254.14.123>
2025-05-22 22:05:26 +0000

    Thu May 22 22:05:26 UTC 2025

--- test/solutions/CHK/r1.js
+++ test/solutions/CHK/r1.js
@@ -8,4 +8,15 @@ describe("CHK challenge: getting total from skus string", function () {
   it("should return 115, when 1 of each item is in the skus string", function () {
     assert.equal(new CheckoutSolution().checkout("ABCD"), 115);
   });
+
+  it("should return -1, when invalid product is in sku", function () {
+    assert.equal(new CheckoutSolution().checkout("Z"), -1);
+  });
+
+  it("should return 130, when 3 A's", function () {
+    assert.equal(new CheckoutSolution().checkout("AAA"), 130);
+  });
+  
+  it("should return ")
 });
+
